1e0c853b932e47c98220d9939efd6550e07b5d3a
Standa Opichal <opichals@gmail.com>
2015-04-23 14:31:55 +0200

    Integrating the Material Design
    - 3rd party from http://callemall.github.io/material-ui/

--- Brocfile.js
+++ Brocfile.js
@@ -2,6 +2,7 @@ var rimraf = require('rimraf'),
     mergeTrees = require('broccoli-merge-trees'),
     Funnel = require('broccoli-funnel'),
     compileSass = require('broccoli-sass'),
+    compileLess = require('broccoli-less-single'),
     babelTranspiler = require('broccoli-babel-transpiler'),
     fastBrowserify = require('broccoli-fast-browserify'),
     uglifyJavaScript = require('broccoli-uglify-js'),
@@ -13,6 +14,9 @@ var staticFiles = new Funnel('src', {
     files: ['index.html']
 });
 
+// material-ui less styles compile
+var materialUICssFiles = compileLess(['src'], 'styles/material-ui.less', 'styles/material-ui.css');
+
 // convert SCSS into CSS
 var cssFiles = compileSass(['src'], 'styles/app.scss', 'styles/app.css');
 
@@ -23,7 +27,7 @@ var jsFiles = new Funnel('src', {
 });
 
 // transpile ES6/7 into ES5
-jsFiles = babelTranspiler(jsFiles, {stage: 0});
+jsFiles = babelTranspiler(jsFiles, {stage: 0, sourceMaps: 'both'});
 
 // transpile for the browser
 jsFiles = fastBrowserify(jsFiles, {
@@ -43,5 +47,5 @@ if (env === 'production') {
 }
 
 rimraf.sync('./dist');
-module.exports = mergeTrees([staticFiles, cssFiles, jsFiles], {overwrite: true});
+module.exports = mergeTrees([staticFiles, cssFiles, materialUICssFiles, jsFiles], {overwrite: true});
 
